7636168aaa08bc876272df5f4d6de52f6417d2cf
ple <homepooh@windowslive.com>
2016-03-06 10:32:31 +0700

    โครงขาดแสดงผลและการดึงแต่ละหน้า ไม่สามารถค้นหาภาษาไทยได้

--- app/src/main/java/appewtc/masterung/smileplant/Searchview.java
+++ app/src/main/java/appewtc/masterung/smileplant/Searchview.java
@@ -4,14 +4,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
-public class Searchview extends AppCompatActivity  {
+public class Searchview extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private ListView listView;
+    private SearchView mSearchView;
+//    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,13 @@ public class Searchview extends AppCompatActivity  {
 
         listView = (ListView) findViewById(R.id.list_view);
         createListView();
+
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+//        mListView = (ListView) findViewById(R.id.list_view);
+//        mListView.setAdapter(new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                mStrings));
+        setupSearchView();
 
     }
 
@@ -45,5 +56,25 @@ public class Searchview extends AppCompatActivity  {
         listView.setAdapter(stringArrayAdapter);
 
     }   // createListView
+
+    private void setupSearchView() {
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQueryHint("Search Here");
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            listView.clearTextFilter();
+        } else {
+            listView.setFilterText(newText.toString());
+        }
+        return true;
+    }
+
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
 
 }
